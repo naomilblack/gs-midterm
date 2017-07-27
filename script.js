d9b7d9acb30d6df049771878a79626f675e51935
Joshua Harshaw <joshuaharshaw@gmail.com>
2017-07-26 21:27:23 -0400

    Most jQuery functionality is completed and form funtionality implemented

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
-var reservations =[
-
-{name:"Joshua", email:"josh@josh.com", seats:[1, 3, 4, 5]}
-]
-
+var reservations =[]
 var selectedSeats = [];
 var reservedSeats = [];
 
 $(".available").click(selectSeat);
 $(".selected").click(removeSeat);
 
-function selectSeat () {
+function selectSeat() {
 	$(this).attr("class", "seat selected");
 	var number = $(this).find("p").text();
 	selectedSeats.push(number);
@@ -18,7 +14,7 @@ function selectSeat () {
   	$(this).bind("click",removeSeat);
 }
 
-function removeSeat () {
+function removeSeat() {
 	$(this).attr("class", "seat available");
 	var number = $(this).find("p").text();
 	var position= selectedSeats.indexOf(number);
@@ -26,4 +22,34 @@ function removeSeat () {
   
   	$(this).bind("click", selectSeat);
   	$(this).unbind("click",removeSeat);
+}
+
+$("#start-form").click(showForm);
+
+function showForm() {
+
+	selectedSeats.forEach(function (seat) {
+		$("#selections").append(seat + " ");
+	});
+
+	$("#form-row").fadeIn();
+	$("#seats-row").hide();
+}
+
+function hideForm() {
+	$("#form-row").hide();
+	$("#seats-row").fadeIn();
+}
+
+$("#final-form").click(finalizeRes);
+
+function finalizeRes() {
+
+	var resName = $("#final-form").find("[name ='name']").val();
+	var resEmail = $("#final-form").find("[name ='email']").val();
+	var reservation = {name: resName, email: resEmail, seats: selectedSeats};
+	reservations.push(reservation);
+
+	selectedSeats = [];
+	//Add here: The code to change the selected items to reserved, and to remove all event listeners. 
 }
