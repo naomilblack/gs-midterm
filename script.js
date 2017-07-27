a301fe7edd2a5d687d7f072d7b49b443f0556819
Joshua Harshaw <joshuaharshaw@gmail.com>
2017-07-27 11:03:43 -0400

    Further jQuery

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-var reservations =[]
-var selectedSeats = [];
-var reservedSeats = [];
+$(document).ready(function () {
+	$(".container").delay(70).fadeIn();
+});
+
+var reservations = [] //Array of objects that hold reservation information
+var selectedSeats = []; // Array of currently selected seats. Erased after every reservation.
+var reservedSeats = [];// Every reserved seat, updated after every reservation.
 
 $(".available").click(selectSeat);
 $(".selected").click(removeSeat);
+$("#final-button").click(finalizeRes);
+$("#start-form").click(showForm);
+$(".form-close").click(hideForm);
 
-function selectSeat() {
+function selectSeat() { // Select an available seat.
 	$(this).attr("class", "seat selected");
 	var number = $(this).find("p").text();
 	selectedSeats.push(number);
@@ -14,19 +21,17 @@ function selectSeat() {
   	$(this).bind("click",removeSeat);
 }
 
-function removeSeat() {
+function removeSeat() { // Remove a selected seat.
 	$(this).attr("class", "seat available");
-	var number = $(this).find("p").text();
+	var number = $(this).find("p").text(); //Seat number, as a string.
 	var position= selectedSeats.indexOf(number);
-  	selectedSeats.splice(position);
+  	selectedSeats.splice(position); // Remove seat to be unselected from the array.
   
-  	$(this).bind("click", selectSeat);
+  	$(this).bind("click", selectSeat);//Remove and add appropriate event listeners.
   	$(this).unbind("click",removeSeat);
 }
 
-$("#start-form").click(showForm);
-
-function showForm() {
+function showForm() { // Hide seat display and show the reservation form.
 
 	selectedSeats.forEach(function (seat) {
 		$("#selections").append(seat + " ");
@@ -36,20 +41,24 @@ function showForm() {
 	$("#seats-row").hide();
 }
 
-function hideForm() {
+function hideForm() { // Show seat display and hide the reservation form.
 	$("#form-row").hide();
 	$("#seats-row").fadeIn();
 }
 
-$("#final-form").click(finalizeRes);
+function finalizeRes() { // Finalize the reservation
 
-function finalizeRes() {
-
-	var resName = $("#final-form").find("[name ='name']").val();
+	var resName = $("#final-form").find("[name ='name']").val(); // Get form values and add them to an object
 	var resEmail = $("#final-form").find("[name ='email']").val();
 	var reservation = {name: resName, email: resEmail, seats: selectedSeats};
 	reservations.push(reservation);
 
+	selectedSeats.forEach(function (seat) { //Push each selected seat into reservedSeats and clear the selectedSeats array.
+		reservedSeats.push(seat);
+	});
+	console.log(reservations, reservedSeats);
 	selectedSeats = [];
-	//Add here: The code to change the selected items to reserved, and to remove all event listeners. 
+
+	$(".selected").unbind("click", removeSeat).attr("class", "seat reserved");// Change class to rserved, remove event listeners.
+	hideForm();
 }
